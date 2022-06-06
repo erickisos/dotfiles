bcb8f65b4e43d7c5187596a64037b2bb13998018
Erick G. Islas-Osuna <erickisos653@gmail.com>
2022-06-06 13:05:25 -0500

    add configs to handle computers

--- .doom.d/nubank.el
+++ .doom.d/nubank.el
@@ -1,10 +1,18 @@
 ;;; ../.dotfiles/.doom.d/nubank.el -*- lexical-binding: t; -*-
 
-(let ((nudev-emacs-path "~/dev/nu/nudev/ides/emacs/"))
-  (when (file-directory-p nudev-emacs-path)
-    (add-to-list 'load-path nudev-emacs-path)
-    (require 'nu nil t)))
-
 (let ((nudev-projects-path "~/dev/nu/"))
   (when (file-directory-p nudev-projects-path)
-    (add-to-list 'projectile-project-search-path nudev-projects-path)))
+    (add-to-list 'projectile-project-search-path nudev-projects-path)
+    (when-let* ((nudev-minimeta-path (concat nudev-projects-path "mini-meta-repo/packages/"))
+                (_directory-p         (file-directory-p nudev-minimeta-path)))
+      (add-to-list 'projectile-project-search-path nudev-minimeta-path))
+    (when-let* ((nudev-emacs-path (concat nudev-projects-path "nudev/ides/emacs/"))
+                (_directory-p      (file-directory-p nudev-emacs-path)))
+      (add-to-list 'load-path nudev-emacs-path)
+      (require 'nu nil t))
+    (when-let* ((nudev-isa-path (concat nudev-projects-path "isa.el"))
+                (_directory-p    (file-directory-p nudev-isa-path)))
+      (use-package! isa
+        :load-path nudev-isa-path
+        :config
+        (map! :leader :desc "isa" "N i" #'isa)))))
