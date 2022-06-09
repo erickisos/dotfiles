d3bda8104545579d273746225d29b72ff8530f6a
Erick G. Islas-Osuna <erickisos653@gmail.com>
2022-06-09 10:56:40 -0500

    doom as literate programming and minor nu config

--- .doom.d/nubank.el
+++ .doom.d/nubank.el
@@ -6,9 +6,13 @@
 
 (when (add-project-path! "~/dev/nu/")
   (add-project-path! "~/dev/nu/mini-meta-repo/packages")
-  (when (file-directory-p "~/dev/nu/nudev/ides/emacs/")
-    (use-package! nu
-      :load-path "~/dev/nu/nudev/ides/emacs/"))
+
+  (let ((nudev-emacs-path "~/dev/nu/nudev/ides/emacs/"))
+    (when (file-directory-p nudev-emacs-path)
+      (add-to-list 'load-path nudev-emacs-path)
+      (require 'nu nil t)
+      (require 'nu-datomic-query nil t)))
+
   (when (file-directory-p "~/dev/nu/isa.el")
     (use-package! isa
       :load-path "~/dev/nu/isa.el")))
--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -1,27 +1,23 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
-
-(package! scss-mode)
 (package! treemacs-all-the-icons)
+
+(unpin! treemacs)
+(unpin! lsp-ui)
+(unpin! lsp-mode)
+(unpin! lsp-treemacs)
+
 (package! paredit)
 (package! posframe)
+(package! scss-mode)
 (package! company-quickhelp)
 
 (unpin! hover)
-(unpin! treemacs)
-(unpin! lsp-treemacs)
-(unpin! lsp-mode)
-(unpin! lsp-ui)
 (unpin! lsp-dart)
 
-(package! map :pin "bb50dba")
-(package! po-mode)
-(package! org-mind-map)
-
-;; Scala config
 (package! lsp-metals)
+(package! map :pin "bb50dba")
+(package! company-tabnine)
 
-;; Spice
 (package! spice-mode)
 (package! ob-spice)
-(package! company-tabnine)
