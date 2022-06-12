911377c3cbf1e60e5db71715e488e438e649c48a
Erick G. Islas-Osuna <erickisos653@gmail.com>
2022-06-12 18:13:58 -0500

    add erc configurations

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -102,4 +102,23 @@
   (setq company-idle-delay        0
         company-show-quick-access t))
 
+(map! :leader
+      (:prefix ("e" . "evaluate/ERC/EWW")
+       :desc "Launch ERC with TLS connection" "E" #'erc-tls))
+
+(setq erc-prompt                  (lambda () (concat "[" (buffer-name) "]"))
+      erc-server                  "irc.libera.chat"
+      erc-nick                    "erickisos"
+      erc-user-full-name          "Erick Isos"
+      erc-track-shorten-start     24
+      erc-autojoin-channels-alist '(("irc.libera.chat"
+                                     "#archlinux"
+                                     "#linux"
+                                     "#emacs"
+                                     "#pyar"))
+      erc-kill-buffer-on-part     t
+      erc-fill-column             100
+      erc-fill-function           'erc-fill-static
+      erc-fill-static-center      16)
+
 (load! "nubank")
