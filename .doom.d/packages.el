3868761b0ac55e4b08a8105ff0f262fc1c0a7e58
Erick G. Islas-Osuna <erickisos653@gmail.com>
2025-09-10 14:22:32 -0600

    chore(doom): use literate config

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -1,33 +1,27 @@
-;; -*- no-byte-compile: t; -*-
-;;; $DOOMDIR/packages.el
-(package! treemacs-all-the-icons)
+;;; $DOOMDIR/packages.el -*- lexical-binding: t; -*-
 
 (unpin! treemacs)
+(unpin! lsp-treemacs)
 (unpin! lsp-ui)
 (unpin! lsp-mode)
-(unpin! lsp-treemacs)
-(unpin! cython-mode)
 
-(package! paredit)
-(package! posframe)
+(package! treemacs-all-the-icons)
 (package! scss-mode)
-;; (package! company-quickhelp)
-
-(unpin! hover)
-(unpin! lsp-dart)
-
 (package! lsp-metals)
-(package! map :pin "bb50dba")
-
-;; (package! spice-mode)
-;; (package! ob-spice)
 
 (package! po-mode
   :recipe (:host github :repo "erickisos/po-mode" :files ("*.el" "Makefile")))
 
-(package! copilot
-   :recipe (:host github :repo "zerolfx/copilot.el" :files ("*.el" "dist")))
-
-(package! docker-tramp)
 (package! ox-moderncv
-    :recipe (:host github :repo "erickisos/org-cv" :files ("*.el")))
+  :recipe (:host github :repo "erickisos/org-cv" :files ("*.el")))
+
+(package! eca
+  :recipe (:host github :repo "editor-code-assistant/eca-emacs" :files ("*.el")))
+
+(package! copilot
+  :recipe (:host github :repo "copilot-emacs/copilot.el" :files ("*.el")))
+
+(package! paredit)
+
+(unpin! lsp-mode)
+(unpin! lsp-ui)
